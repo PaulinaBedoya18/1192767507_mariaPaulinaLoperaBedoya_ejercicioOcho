68f7f936152241792b102c5e6b70a09b1504bf4b
Maria Paulina Lopera <mloperap@cesde.net>
2024-10-27 23:19:12 -0500

    Teminación de calculos para ejercicio

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function iniciarSesion() {
 
         if (usuario === usuarioCorrecto && contrasena === contrasenaCorrecta) {
             console.log("Inicio de sesión exitoso.");
-            calcularSalarioVariosEmpleados();
+            calcularSalarioEmpleados();
             return;
         } else {
             intentos++;
@@ -19,14 +19,37 @@ function iniciarSesion() {
     alert("Completaste 3 intentos fallidos. El sistema se cerrará.");
 }
 
-function calcularSalarioVariosEmpleados() {
-    let continuar = true; 
+function calcularSalarioEmpleados() {
+    let continuar = true;
 
-    while (continuar) { 
-        let horasTrabajadas = capturarHorasTrabajo(); 
-        let salario = calcularSalario(horasTrabajadas); 
-        mostrarSalario(salario); 
+    while (continuar) {
+        let horasTrabajadas = capturarHorasTrabajo();
+        let salario = calcularSalario(horasTrabajadas);
+        mostrarSalario(salario);
 
-        continuar = confirm("¿Desea calcular el salario de otro empleado?"); 
+        continuar = confirm("¿Desea calcular el salario de otro empleado?");
     }
+}
+
+function capturarHorasTrabajo() {
+    let horas = prompt("Ingrese las horas trabajadas por el empleado: ");
+    return parseInt(horas);
+}
+
+function calcularSalario(horasTrabajadas) {
+    let salario;
+
+    if (horasTrabajadas < 40) {
+        salario = horasTrabajadas * 20000;
+    } else if (horasTrabajadas <= 50) {
+        salario = (40 * 20000) + ((horasTrabajadas - 40) * 25000);
+    } else {
+        salario = (40 * 20000) + (10 * 25000) + ((horasTrabajadas - 50) * 30000);
+    }
+
+    return salario;
+}
+
+function mostrarSalario(salario) {
+    document.getElementById('resultado').innerText = "El salario del empleado es: $" + salario;
 }
